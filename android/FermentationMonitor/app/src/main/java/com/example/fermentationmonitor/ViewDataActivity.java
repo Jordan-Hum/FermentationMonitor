103f946f77f60ce69c7bf95aabc0a98ffacd6336
Anthony I <a.iacampo99@gmail.com>
2020-10-29 13:49:22 -0400

    Implemented a basic UI to display all fetched data in real-time

---
+++ android/FermentationMonitor/app/src/main/java/com/example/fermentationmonitor/ViewDataActivity.java
@@ -0,0 +1,14 @@
+package com.example.fermentationmonitor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewDataActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_data);
+    }
+}
